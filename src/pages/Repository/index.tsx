c5751c18abe5112dcdf06dad3b26c2c25fc5da64
Matheus Fraga <matheuslf44@gmail.com>
2020-09-11 00:16:41 -0300

    page navigation done :tada:

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+const Dashboard: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <h1>Repository {params.repository}</h1>
+  )
+}
+
+export default Dashboard;
